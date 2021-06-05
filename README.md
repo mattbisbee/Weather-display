ee14aa055666f7510069d60145fed78e8ee53566
Biznaz <aldhelm7@gmail.com>
2021-06-05 11:37:21 -0700

    recent search for loop working

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Weather-display
+# Weather-display# Code Quiz
+
+## Display the current weather for a selected city and it's 5 day forecast
+---
+### **General Info:**
+
+
+
+### **Requirements**
+
+
+
+### **Final Notes**
+
+
+### **Location:**
+Website can be found at : https://mattbisbee.github.io/Weather-display/
+
+GitHub: https://github.com/mattbisbee/Weather-display/
+
+## **Images:**
+![Screenshot](assets/capture.jpg)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+
 function searchCity() {
-  var cityName = document.querySelector('#cityName').value;
+  
+  let cityName = document.querySelector('#cityName').value;
 
   //setting time for the 5 day forecast
   currentDay = moment().format('dddd, MMMM Do YYYY');
@@ -19,7 +21,6 @@ function searchCity() {
       .then(function(response) {
       
         //today's data
-        var searchData = [];
 
         var currentCity = response.name;
         var currentCityEl = document.querySelector('#currentCity');
@@ -27,145 +28,165 @@ function searchCity() {
 
         var currentDayEl = document.querySelector('#currentDay');
         currentDayEl.innerHTML = currentDay;
-        searchData.push(currentDay);
 
         var currentTemp = response.main.temp;
         var currentTempEl = document.querySelector('#current0');
         currentTempEl.innerHTML = 'Current Temp: ' + currentTemp + ' ℉';
-        searchData.push(currentTemp);
 
         var currentWind = response.wind.speed;
         var currentWindEl = document.querySelector('#wind0');
         currentWindEl.innerHTML = 'Wind Speed: ' + currentWind + ' mph';
-        searchData.push(currentWind);
 
         var currentHumid = response.main.humidity;
         var currentHumidEl = document.querySelector('#humidity0');
         currentHumidEl.innerHTML = 'Humidity: ' + currentHumid + '%';
-        searchData.push(currentHumid);
 
         var latitude = response.coord.lat;
-        searchData.push(latitude);
 
         var longitude = response.coord.lon;
-        searchData.push(longitude)
 
-        //push data to local storage
-        localStorage.setItem(currentCity, searchData);
-        console.log(searchData);
+        localStorage.setItem(currentCity,currentCity);
+      
+        var values = [];
 
-        //display saved info
-    
-    fetch(
-    'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&units=imperial&appid=47cb69761d77bd9169b3d2a6130a2f5f'
-      )
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(response) {
+        function allStorage() {
+          keys= Object.keys(localStorage),
+          i=keys.length;
+
+          while (i--) {
+            values.push(localStorage.getItem(keys[i]))
+          }
+          console.log(values);
+          return values;
+        }
+        allStorage();
+
         
+        var recentSearchDiv = document.getElementById('recentSearches');
 
-    
-        //tomorrow (day 1 forcast)
-        var day1El = document.querySelector('#header1');
-        day1El.innerHTML = day1;
-
-        var day1High = response.daily[1].temp.max;
-        var day1HighEl = document.querySelector('#high1');
-        day1HighEl.innerHTML = 'High: ' + day1High + ' ℉';
-
-        var day1Low = response.daily[1].temp.min;
-        var day1LowEl = document.querySelector('#low1');
-        day1LowEl.innerHTML = 'Low: ' + day1Low + ' ℉';
-
-        var day1Wind = response.daily[1].wind_speed;
-        var day1WindEl = document.querySelector('#wind1');
-        day1WindEl.innerHTML = 'Wind: ' + day1Wind + ' mph';
-
-        var day1Humid = response.daily[1].humidity;
-        var day1HumidEl = document.querySelector('#humidity1');
-        day1HumidEl.innerHTML = 'Humidity: ' + day1Humid + '%';
+        
+        for (i = 0; i < values.length; i++) {
+          $('#searchBtn').click(function() {
+            $('#recentSearches').empty();
+          });
+          var createBtn = document.createElement('button');
+          createBtn.setAttribute('id', 'recentSearchBtn');
+          createBtn.innerHTML = values[i];
+          recentSearchDiv.appendChild(createBtn);
+        }
 
 
-        //day 2 forcast
-        var day2El = document.querySelector('#header2');
-        day2El.innerHTML = day2;
 
-        var day2High = response.daily[2].temp.max;
-        var day2HighEl = document.querySelector('#high2');
-        day2HighEl.innerHTML = 'High: ' + day2High + ' ℉';
-
-        var day2Low = response.daily[2].temp.min;
-        var day2LowEl = document.querySelector('#low2');
-        day2LowEl.innerHTML = 'Low: ' + day2Low + ' ℉';
-
-        var day2Wind = response.daily[2].wind_speed;
-        var day2WindEl = document.querySelector('#wind2');
-        day2WindEl.innerHTML = 'Wind: ' + day2Wind + ' mph';
-
-        var day2Humid = response.daily[2].humidity;
-        var day2HumidEl = document.querySelector('#humidity2');
-        day2HumidEl.innerHTML = 'Humidity: ' + day2Humid + '%';
-
-        //day 3 forcast
-        var day3El = document.querySelector('#header3');
-        day3El.innerHTML = day3;
-
-        var day3High = response.daily[3].temp.max;
-        var day3HighEl = document.querySelector('#high3');
-        day3HighEl.innerHTML = 'High: ' + day3High + ' ℉';
-
-        var day3Low = response.daily[3].temp.min;
-        var day3LowEl = document.querySelector('#low3');
-        day3LowEl.innerHTML = 'Low: ' + day3Low + ' ℉';
-
-        var day3Wind = response.daily[3].wind_speed;
-        var day3WindEl = document.querySelector('#wind3');
-        day3WindEl.innerHTML = 'Wind: ' + day3Wind + ' mph';
-
-        var day3Humid = response.daily[3].humidity;
-        var day3HumidEl = document.querySelector('#humidity3');
-        day3HumidEl.innerHTML = 'Humidity: ' + day3Humid + '%';
-
-        //day 4 forcast
-        var day4El = document.querySelector('#header4');
-        day4El.innerHTML = day4;
-
-        var day4High = response.daily[4].temp.max;
-        var day4HighEl = document.querySelector('#high4');
-        day4HighEl.innerHTML = 'High: ' + day4High + ' ℉';
-
-        var day4Low = response.daily[4].temp.min;
-        var day4LowEl = document.querySelector('#low4');
-        day4LowEl.innerHTML = 'Low: ' + day4Low + ' ℉';
-
-        var day4Wind = response.daily[4].wind_speed;
-        var day4WindEl = document.querySelector('#wind4');
-        day4WindEl.innerHTML = 'Wind: ' + day4Wind + ' mph';
-
-        var day4Humid = response.daily[4].humidity;
-        var day4HumidEl = document.querySelector('#humidity4');
-        day4HumidEl.innerHTML = 'Humidity: ' + day4Humid + '%';
-
-        //day 5 forcast
-        var day5El = document.querySelector('#header5');
-        day5El.innerHTML = day5;
-
-        var day5High = response.daily[5].temp.max;
-        var day5HighEl = document.querySelector('#high5');
-        day5HighEl.innerHTML = 'High: ' + day5High + ' ℉';
-
-        var day5Low = response.daily[5].temp.min;
-        var day5LowEl = document.querySelector('#low5');
-        day5LowEl.innerHTML = 'Low: ' + day5Low + ' ℉';
-
-        var day5Wind = response.daily[5].wind_speed;
-        var day5WindEl = document.querySelector('#wind5');
-        day5WindEl.innerHTML = 'Wind: ' + day5Wind + ' mph';
-
-        var day5Humid = response.daily[5].humidity;
-        var day5HumidEl = document.querySelector('#humidity5');
-        day5HumidEl.innerHTML = 'Humidity: ' + day5Humid + '%';
-      });
-    });
-    }
+        fetch(
+          'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&units=imperial&appid=47cb69761d77bd9169b3d2a6130a2f5f'
+          )
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(response) {
+            
+            
+            
+            //tomorrow (day 1 forcast)
+            var day1El = document.querySelector('#header1');
+            day1El.innerHTML = day1;
+            
+            var day1High = response.daily[1].temp.max;
+            var day1HighEl = document.querySelector('#high1');
+            day1HighEl.innerHTML = 'High: ' + day1High + ' ℉';
+            
+            var day1Low = response.daily[1].temp.min;
+            var day1LowEl = document.querySelector('#low1');
+            day1LowEl.innerHTML = 'Low: ' + day1Low + ' ℉';
+            
+            var day1Wind = response.daily[1].wind_speed;
+            var day1WindEl = document.querySelector('#wind1');
+            day1WindEl.innerHTML = 'Wind: ' + day1Wind + ' mph';
+            
+            var day1Humid = response.daily[1].humidity;
+            var day1HumidEl = document.querySelector('#humidity1');
+            day1HumidEl.innerHTML = 'Humidity: ' + day1Humid + '%';
+            
+            
+            //day 2 forcast
+            var day2El = document.querySelector('#header2');
+            day2El.innerHTML = day2;
+            
+            var day2High = response.daily[2].temp.max;
+            var day2HighEl = document.querySelector('#high2');
+            day2HighEl.innerHTML = 'High: ' + day2High + ' ℉';
+            
+            var day2Low = response.daily[2].temp.min;
+            var day2LowEl = document.querySelector('#low2');
+            day2LowEl.innerHTML = 'Low: ' + day2Low + ' ℉';
+            
+            var day2Wind = response.daily[2].wind_speed;
+            var day2WindEl = document.querySelector('#wind2');
+            day2WindEl.innerHTML = 'Wind: ' + day2Wind + ' mph';
+            
+            var day2Humid = response.daily[2].humidity;
+            var day2HumidEl = document.querySelector('#humidity2');
+            day2HumidEl.innerHTML = 'Humidity: ' + day2Humid + '%';
+            
+            //day 3 forcast
+            var day3El = document.querySelector('#header3');
+            day3El.innerHTML = day3;
+            
+            var day3High = response.daily[3].temp.max;
+            var day3HighEl = document.querySelector('#high3');
+            day3HighEl.innerHTML = 'High: ' + day3High + ' ℉';
+            
+            var day3Low = response.daily[3].temp.min;
+            var day3LowEl = document.querySelector('#low3');
+            day3LowEl.innerHTML = 'Low: ' + day3Low + ' ℉';
+            
+            var day3Wind = response.daily[3].wind_speed;
+            var day3WindEl = document.querySelector('#wind3');
+            day3WindEl.innerHTML = 'Wind: ' + day3Wind + ' mph';
+            
+            var day3Humid = response.daily[3].humidity;
+            var day3HumidEl = document.querySelector('#humidity3');
+            day3HumidEl.innerHTML = 'Humidity: ' + day3Humid + '%';
+            
+            //day 4 forcast
+            var day4El = document.querySelector('#header4');
+            day4El.innerHTML = day4;
+            
+            var day4High = response.daily[4].temp.max;
+            var day4HighEl = document.querySelector('#high4');
+            day4HighEl.innerHTML = 'High: ' + day4High + ' ℉';
+            
+            var day4Low = response.daily[4].temp.min;
+            var day4LowEl = document.querySelector('#low4');
+            day4LowEl.innerHTML = 'Low: ' + day4Low + ' ℉';
+            
+            var day4Wind = response.daily[4].wind_speed;
+            var day4WindEl = document.querySelector('#wind4');
+            day4WindEl.innerHTML = 'Wind: ' + day4Wind + ' mph';
+            
+            var day4Humid = response.daily[4].humidity;
+            var day4HumidEl = document.querySelector('#humidity4');
+            day4HumidEl.innerHTML = 'Humidity: ' + day4Humid + '%';
+            
+            //day 5 forcast
+            var day5El = document.querySelector('#header5');
+            day5El.innerHTML = day5;
+            
+            var day5High = response.daily[5].temp.max;
+            var day5HighEl = document.querySelector('#high5');
+            day5HighEl.innerHTML = 'High: ' + day5High + ' ℉';
+            
+            var day5Low = response.daily[5].temp.min;
+            var day5LowEl = document.querySelector('#low5');
+            day5LowEl.innerHTML = 'Low: ' + day5Low + ' ℉';
+            
+            var day5Wind = response.daily[5].wind_speed;
+            var day5WindEl = document.querySelector('#wind5');
+            day5WindEl.innerHTML = 'Wind: ' + day5Wind + ' mph';
+            
+            var day5Humid = response.daily[5].humidity;
+            var day5HumidEl = document.querySelector('#humidity5');
+            day5HumidEl.innerHTML = 'Humidity: ' + day5Humid + '%';
+          });
+        });
+      }
